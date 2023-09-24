86d57d461137edd8b4459c383e5f2843745b2441
ozmen1 <ozmen41@gmail.com>
2023-09-24 13:31:06 +0300

    Add project files.

---
+++ DataTable/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using DataTable.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DataTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(GetProducts());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> productList = new List<Product>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Product product = new Product();
+
+                product.Code = i.ToString();
+                product.Name = "PRODUCT - " + i;
+                product.BarcodeId = "BARCODE #" + i;
+
+                productList.Add(product);
+            }
+
+            return productList;
+        }
+    }
+}
---
+++ DataTable/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace DataTable.Models
+{
+    public class Product
+    {
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Ürün kodu")]
+        public string Code { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DisplayName("Ürün adı")]
+        public string Name { get; set; }
+
+        [DisplayName("Barkod")]
+        public string BarcodeId { get; set; }
+
+    }
+}
